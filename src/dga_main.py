7c375923b515196c0decf6cfd9192c16a6b2a291
julpe <julian.peil@gmx.at>
2025-04-02 15:14:45 +0200

    Stack smashing issue?

--- src/dga_main.py
+++ src/dga_main.py
@@ -25,7 +25,7 @@ def execute_dga_routine():
     if comm.rank == 0:
         g_dmft, sigma_dmft, g2_dens, g2_magn = dga_io.load_from_w2dyn_file_and_update_config()
     else:
-        g_dmft, sigma_dmft, g2_dens, g2_magn = (None,) * 4
+        g_dmft, sigma_dmft, g2_dens, g2_magn = None, None, None, None
 
     logger.log_info("Config init and folder setup done.")
     logger.log_info("Loaded data from w2dyn file.")
@@ -62,7 +62,17 @@ def execute_dga_routine():
             local_sde.perform_local_schwinger_dyson(giwk, g2_dens, g2_magn, u_loc)
         )
     else:
-        gamma_dens, gamma_magn, chi_dens, chi_magn, vrg_dens, vrg_magn, f_dens, f_magn, sigma_local = (None,) * 9
+        gamma_dens, gamma_magn, chi_dens, chi_magn, vrg_dens, vrg_magn, f_dens, f_magn, sigma_local = (
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
 
     gamma_dens, gamma_magn, chi_dens, chi_magn, vrg_dens, vrg_magn, f_dens, f_magn, sigma_local = comm.bcast(
         (gamma_dens, gamma_magn, chi_dens, chi_magn, vrg_dens, vrg_magn, f_dens, f_magn, sigma_local)
