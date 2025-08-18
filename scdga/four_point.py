Fixed rotation of bands

scdga/four_point.py
@@ -525,14 +525,17 @@ class FourPoint(LocalFourPoint, IAmNonLocal):
             raise ValueError("Rotating the orbitals is only allowed for objects that have two bands.")
 
         copy = deepcopy(self)
-        r = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+
+        if theta == 0:
+            return copy
+
+        r = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
         einsum_str = (
-            "pi,qj,rk,sl,xpqrs...->xijkl..."
+            "ip,jq,rk,sl,xpqrs...->xijkl..."
             if self.has_compressed_q_dimension
-            else "pi,qj,rk,sl,xyzpqrs...->xyzijkl..."
+            else "ip,jq,rk,sl,xyzpqrs...->xyzijkl..."
         )
-        copy.mat = np.einsum(einsum_str, np.conj(r.T), np.conj(r.T), r, r, copy.mat, optimize=True)
-        copy.mat[np.abs(copy.mat) < 1e-13] = 0
+        copy.mat = np.einsum(einsum_str, r.T, r.T, r, r, copy.mat, optimize=True)
         return copy
 
     @staticmethod

scdga/greens_function.py
@@ -184,21 +184,22 @@ class GreensFunction(LocalNPoint, IAmNonLocal):
         Rotates the orbitals of the four-point object around the angle :math:`\theta`. :math:`\theta` must be given in
         radians and the number of orbitals needs to be 2.
         """
-        r = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+        if self.n_bands != 2:
+            raise ValueError("Rotating the orbitals is only allowed for objects that have two bands.")
 
         copy = deepcopy(self)
 
-        if len(self.current_shape) == 3:  # [o1,o2,v]
-            copy.mat = np.einsum("ab,cd,acv->bdv", np.conj(r.T), r, copy.mat, optimize=True)
-            copy.mat[np.abs(copy.mat) < 1e-13] = 0
+        if theta == 0:
             return copy
 
-        if copy.n_bands != 2:
-            raise ValueError("Rotating the orbitals is only allowed for objects that have two bands.")
+        r = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
 
-        einsum_str = "pi,qj,xpq...->xij..." if self.has_compressed_q_dimension else "pi,qj,xyzpq...->xyzij..."
-        copy.mat = np.einsum(einsum_str, np.conj(r.T), r, copy.mat, optimize=True)
-        copy.mat[np.abs(copy.mat) < 1e-13] = 0
+        if len(self.current_shape) == 3:  # [o1,o2,v]
+            copy.mat = np.einsum("ip,qj,pqv->ijv", r.T, r, copy.mat, optimize=True)
+            return copy
+
+        einsum_str = "ip,qj,xpqv->xijv" if self.has_compressed_q_dimension else "ip,qj,xyzpqv->xyzijv"
+        copy.mat = np.einsum(einsum_str, r.T, r, copy.mat, optimize=True)
         return copy
 
     def _get_fill_nonlocal(self) -> tuple[float, np.ndarray, np.ndarray]:

scdga/interaction.py
@@ -31,7 +31,7 @@ class LocalInteraction(IHaveMat, IHaveChannel):
             raise ValueError("Invalid permutation.")
 
         if split[0] == split[1]:
-            return self
+            return deepcopy(self)
 
         return LocalInteraction(np.einsum(permutation, self.mat, optimize=True), self.channel)
 
@@ -44,9 +44,13 @@ class LocalInteraction(IHaveMat, IHaveChannel):
             raise ValueError("Rotating the orbitals is only allowed for objects that have two bands.")
 
         copy = deepcopy(self)
-        r = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
-        copy.mat = np.einsum("pi,qj,rk,sl,pqrs->ijkl", np.conj(r.T), np.conj(r.T), r, r, copy.mat, optimize=True)
+        if theta == 0:
+            return copy
+
+        r = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+
+        copy.mat = np.einsum("ip,jq,rk,sl,pqrs->ijkl", r.T, r.T, r, r, copy.mat, optimize=True)
         return copy
 
     def as_channel(self, channel: SpinChannel) -> "LocalInteraction":
@@ -167,7 +171,7 @@ class Interaction(LocalInteraction, IAmNonLocal):
             raise ValueError("Invalid permutation.")
 
         if split[0] == split[1]:
-            return self
+            return deepcopy(self)
 
         permutation = f"...{split[0]}->...{split[1]}"
         return Interaction(
@@ -183,10 +187,13 @@ class Interaction(LocalInteraction, IAmNonLocal):
             raise ValueError("Rotating the orbitals is only allowed for objects that have two bands.")
 
         copy = deepcopy(self)
-        r = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
-        copy.mat = np.einsum("pi,qj,rk,sl,...pqrs->...ijkl", np.conj(r.T), np.conj(r.T), r, r, copy.mat, optimize=True)
-        copy.mat[np.abs(copy.mat) < 1e-13] = 0
+        if theta == 0:
+            return copy
+
+        r = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+
+        copy.mat = np.einsum("ip,jq,rk,sl,...pqrs->...ijkl", r.T, r.T, r, r, copy.mat, optimize=True)
         return copy
 
     def as_channel(self, channel: SpinChannel) -> "Interaction":

scdga/local_four_point.py
@@ -687,10 +687,13 @@ class LocalFourPoint(LocalNPoint, IHaveChannel):
             raise ValueError("Rotating the orbitals is only allowed for objects that have two bands.")
 
         copy = deepcopy(self)
-        r = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
-        copy.mat = np.einsum("pi,qj,rk,sl,pqrs...->ijkl...", np.conj(r.T), np.conj(r.T), r, r, copy.mat, optimize=True)
-        copy.mat[np.abs(copy.mat) < 1e-13] = 0
+        if theta == 0:
+            return copy
+
+        r = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+
+        copy.mat = np.einsum("ip,jq,rk,sl,pqrs...->ijkl...", r.T, r.T, r, r, copy.mat, optimize=True)
         return copy
 
     @staticmethod

scdga/nonlocal_sde.py
@@ -40,7 +40,17 @@ def get_hartree_fock(
 
     hartree = 2 * (u_loc + v_q0).times("qabcd,dc->ab", config.sys.occ)
     fock = -1.0 / nq_tot * (u_loc + v_nonloc).compress_q_dimension().times("qadcb,qkdc->kab", occ_qk)
-    return hartree[None, ..., None], fock[..., None]  # [k,o1,o2,v]
+    hartree, fock = hartree[None, ..., None], fock[..., None]  # [k,o1,o2,v]
+
+    theta = 0
+
+    if theta == 0:
+        return hartree, fock
+
+    r = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+    hartree = np.einsum("ip,qj,xpqv->xijv", r.T, r, hartree, optimize=True)
+    fock = np.einsum("ip,qj,xpqv->xijv", r.T, r, fock, optimize=True)
+    return hartree, fock
 
 
 def create_auxiliary_chi_r_q(
@@ -402,11 +412,11 @@ def calculate_self_energy_q(
         logger.log_info(f"Starting iteration {current_iter}.")
         logger.log_info("----------------------------------------")
 
-        # giwk_full = GreensFunction.get_g_full(
-        #    sigma_old.rotate_orbitals(theta=-theta), config.sys.mu, config.lattice.hamiltonian.get_ek()
-        # ).rotate_orbitals(theta=theta)
+        giwk_full = GreensFunction.get_g_full(
+            sigma_old.rotate_orbitals(theta=-theta), config.sys.mu, config.lattice.hamiltonian.get_ek()
+        ).rotate_orbitals(theta=theta)
 
-        giwk_full = GreensFunction.get_g_full(sigma_old, config.sys.mu, config.lattice.hamiltonian.get_ek())
+        # giwk_full = GreensFunction.get_g_full(sigma_old, config.sys.mu, config.lattice.hamiltonian.get_ek())
         # giwk_full.mat = giwk_full.mat[..., 0, 0, :][..., None, None, :]
         giwk_full.save(output_dir=config.output.output_path, name="g_dga")
 
@@ -436,10 +446,7 @@ def calculate_self_energy_q(
         logger.log_memory_usage("Gchi0_q_inv", gchi0_q_core_inv, comm.size)
 
         if config.eliashberg.perform_eliashberg:
-            gchi0_q_core_inv.save(
-                name=f"gchi0_q_inv_rank_{comm.rank}",
-                output_dir=config.output.eliashberg_path,
-            )
+            gchi0_q_core_inv.save(name=f"gchi0_q_inv_rank_{comm.rank}", output_dir=config.output.eliashberg_path)
 
         gchi0_q_core_sum = 1.0 / config.sys.beta * gchi0_q_core.sum_over_all_vn(config.sys.beta)
         del gchi0_q_core
@@ -447,14 +454,13 @@ def calculate_self_energy_q(
         kernel += calculate_sigma_kernel_r_q(
             gamma_dens, gchi0_q_core_inv, gchi0_q_full_sum, gchi0_q_core_sum, u_loc, v_nonloc, mpi_dist_irrk
         )
-        del gamma_dens
         comm.Barrier()
         logger.log_info("Calculated kernel for density channel.")
 
         kernel += 3 * calculate_sigma_kernel_r_q(
             gamma_magn, gchi0_q_core_inv, gchi0_q_full_sum, gchi0_q_core_sum, u_loc, v_nonloc, mpi_dist_irrk
         )
-        del gchi0_q_core_inv, gchi0_q_full_sum, gchi0_q_core_sum, gamma_magn
+        del gchi0_q_core_inv, gchi0_q_full_sum, gchi0_q_core_sum
         comm.Barrier()
         logger.log_info("Calculated kernel for magnetic channel.")
 
@@ -481,7 +487,7 @@ def calculate_self_energy_q(
         # This is done to minimize noise. We remove some fluctuations from dmft that are included in the local self-energy
         # calculated in this code and add the smooth dmft self-energy
         sigma_new += delta_sigma
-        sigma_new = sigma_new.concatenate_self_energies(sigma_dmft)  # .rotate_orbitals(theta=-theta)
+        sigma_new = sigma_new.concatenate_self_energies(sigma_dmft).rotate_orbitals(theta=-theta)
 
         if comm.rank == 0:
             count_nonzero_orbital_entries(sigma_new, "sigma_new")

scdga/self_energy.py
@@ -188,10 +188,14 @@ class SelfEnergy(LocalNPoint, IAmNonLocal):
             raise ValueError("Rotating the orbitals is only allowed for objects that have two bands.")
 
         copy = deepcopy(self)
-        r = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
-        einsum_str = "pi,qj,xpq...->xij..." if self.has_compressed_q_dimension else "pi,qj,xyzpq...->xyzij..."
-        copy.mat = np.einsum(einsum_str, np.conj(r.T), r, copy.mat, optimize=True)
-        copy.mat[np.abs(copy.mat) < 1e-13] = 0
+
+        if theta == 0:
+            return copy
+
+        r = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+
+        einsum_str = "ip,qj,xpq...->xij..." if self.has_compressed_q_dimension else "ip,qj,xyzpq...->xyzij..."
+        copy.mat = np.einsum(einsum_str, r.T, r, copy.mat, optimize=True)
         return copy
 
     def _estimate_niv_core(self, err: float = 1e-5):